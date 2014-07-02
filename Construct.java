56a659a093a99c55a5e3f0be94035b3b0b227d0d
Dustin-Walker <duwalker88@gmail.com>
2014-07-02 00:38:04 -0400

    All methods are written. A logical error exists which is causing the Cronbach's Alpha to calculate incorrect values.

--- Construct.java
+++ Construct.java
@@ -3,8 +3,12 @@ package com.company;
 import java.util.ArrayList;
 
 /**
- * Created by User on 6/23/2014.
+ * Created by Dustin Walker on 6/23/2014.
+ * This class represents the concepts of constructs as they relate to the
+ * Cronbach's Alpha statistic. The construct consists of a name with
+ * integer values that represent responses to questions.
  */
+
 public class Construct {
 
     public Construct(String name){
@@ -12,24 +16,28 @@ public class Construct {
     }
 
     private String name = "";
+
+    public ArrayList<Integer> getScoreList() {
+        return scoreList;
+    }
+
     private ArrayList<Integer> scoreList = new ArrayList<Integer>();
 
     public void addToList(String[] lineInput){
+        /**
+         * @param lineInput The line being read in from the formatted CSV file
+         */
         for(int i = 2; i < lineInput.length; i++)
             scoreList.add(Integer.parseInt(lineInput[i]));
     }
 
-    public int summation(){
-        int total=0;
-        for(int i : scoreList)
-             total+= i;
-        return total;
-    }
-
     public void printScoreList(){
+        /**
+         * This function is used for testing purposes.
+         */
         System.out.print(name+": ");
-        for(int i = 0; i < scoreList.size(); i++)
-            System.out.print(scoreList.get(i) + ",");
+        for (Integer aScoreList : scoreList)
+            System.out.print(aScoreList + ",");
         System.out.println();
     }
 
@@ -41,11 +49,37 @@ public class Construct {
         this.name = name;
     }
 
-	//public int variation(){
-	
-    //}
+	public double variance(){
+        /**
+         * Variance measures the spread of a set of numbers.
+         * Higher variance means greater spread.
+         * The algorithm being used is translated from this page:
+         * http://en.wikipedia.org/wiki/Algorithms_for_calculating_variance
+         * This is the two-pass compensated-summation algorithm.
+         * @return variance of integers
+         */
+        double n=0, sum1=0, sum2=0, sum3=0;
+        for(int i : scoreList){
+            n += 1;
+            sum1 += i;
+        }
+        double sampleMean = sum1/n;
+        for(int i : scoreList){
+            sum2 += Math.pow(i - sampleMean, 2);
+            sum3 += i-sampleMean;
+        }
+        return (Math.pow(sum2-sum3,2)/n)/(n-1);
+    }
 
-    //public int square_variation(){
+    public double variance2(){
 
-    //}
+        double n=0,sum=0,sum_sqr=0;
+        for(int i : scoreList){
+            n += 1;
+            sum += i;
+            sum_sqr += i*i;
+        }
+        return (sum_sqr - (sum*sum)/n)/(n-1);
+    }
+
 }
--- Main.java
+++ Main.java
@@ -19,7 +19,7 @@ public class Main {
             ArrayList<String> constructStringList = new ArrayList<String>();
             while(fileScanner.hasNextLine()){
                 lineReader = fileScanner.nextLine().split(",");
-                System.out.println(lineReader[0]+" "+lineReader[1]);
+                //System.out.println(lineReader[0]+" "+lineReader[1]);
                 if(constructStringList.contains(lineReader[1])) {
                    for(Construct c : constructList)
                        if(c.getName().equals(lineReader[1]))
@@ -32,13 +32,75 @@ public class Main {
                             c.addToList(lineReader);
                 }
             }
-            for(Construct c : constructList)
-                c.printScoreList();
-
+            System.out.println("Cronbach's Alpha for this data set is: "+cronbachAlpha(constructList));
         } finally {
 
         }
     }
 
-}
+    public static double cronbachAlpha(ArrayList<Construct> constructList){
+        /**
+         * Cronbach's Alpha is a measure of internal consistency.
+         * Equation used can be found here:
+         * http://en.wikipedia.org/wiki/Cronbach's_alpha
+         * @param constructList List of construct objects
+         * @return Cronbach's Alpha
+         */
 
+        //Construct the numerator
+        double testletVariance = 0;
+        for(Construct c : constructList) {
+            testletVariance += c.variance();
+            System.out.println(c.getName()+" variance = "+c.variance());
+        }
+        System.out.println("Numerator = "+testletVariance);
+
+        //Construct the denominator
+        double testVariance;
+        ArrayList<Integer> responseList = new ArrayList<Integer>();
+        for(Construct c : constructList){
+            for(int i : c.getScoreList())
+                responseList.add(i);
+        }
+        testVariance = variance(responseList);
+        System.out.println("Denominator contains "+responseList.size()+" items and = "+testVariance);
+
+        int k = constructList.size();
+        System.out.println("K = "+k);
+        return (k / (k-1)) * (1-testletVariance/testVariance);
+    }
+
+    public static double variance(ArrayList<Integer> responseList){
+        /**
+         * Variance measures the spread of a set of numbers.
+         * Higher variance means greater spread.
+         * The algorithm being used is translated from this page:
+         * http://en.wikipedia.org/wiki/Algorithms_for_calculating_variance
+         * This is the two-pass compensated-summation algorithm.
+         * @return variance of integers
+         */
+        double n=0, sum1=0, sum2=0, sum3=0;
+        for(int i : responseList){
+            n += 1;
+            sum1 += i;
+        }
+        double sampleMean = sum1/n;
+        for(int i : responseList){
+            sum2 += Math.pow(i - sampleMean, 2);
+            sum3 += i-sampleMean;
+        }
+        return (Math.pow(sum2-sum3,2)/n)/(n-1);
+    }
+
+    public static double variance2(ArrayList<Integer> responseList){
+
+        double n=0,sum=0,sum_sqr=0;
+        for(int i : responseList){
+            n += 1;
+            sum += i;
+            sum_sqr += i*i;
+        }
+        return (sum_sqr - (sum*sum)/n)/(n-1);
+    }
+
+}
