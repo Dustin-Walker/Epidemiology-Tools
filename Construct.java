195cab2eb728194703f5186417a9575e0a6400d0
Dustin-Walker <duwalker88@gmail.com>
2014-07-01 21:53:03 -0400

    Trying this again.

--- Construct.java
+++ Construct.java
@@ -1,7 +1,51 @@
 package com.company;
 
+import java.util.ArrayList;
+
 /**
  * Created by User on 6/23/2014.
  */
 public class Construct {
+
+    public Construct(String name){
+        setName(name);
+    }
+
+    private String name = "";
+    private ArrayList<Integer> scoreList = new ArrayList<Integer>();
+
+    public void addToList(String[] lineInput){
+        for(int i = 2; i < lineInput.length; i++)
+            scoreList.add(Integer.parseInt(lineInput[i]));
+    }
+
+    public int summation(){
+        int total=0;
+        for(int i : scoreList)
+             total+= i;
+        return total;
+    }
+
+    public void printScoreList(){
+        System.out.print(name+": ");
+        for(int i = 0; i < scoreList.size(); i++)
+            System.out.print(scoreList.get(i) + ",");
+        System.out.println();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+	//public int variation(){
+	
+    //}
+
+    //public int square_variation(){
+
+    //}
 }
--- Main.java
+++ Main.java
@@ -1,9 +1,44 @@
 package com.company;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-        test;
+    public static void main(String[] args) throws IOException {
+        //Read the file in through standard input
+        Scanner fileScanner;
+        try{
+            fileScanner = new Scanner(new FileReader("C:\\Programming\\Cronbach's Alpha\\sample1.in"));
+            fileScanner.useDelimiter("\n");
+            fileScanner.nextLine();
+            String[] lineReader;
+            ArrayList<Construct> constructList = new ArrayList<Construct>();
+            ArrayList<String> constructStringList = new ArrayList<String>();
+            while(fileScanner.hasNextLine()){
+                lineReader = fileScanner.nextLine().split(",");
+                System.out.println(lineReader[0]+" "+lineReader[1]);
+                if(constructStringList.contains(lineReader[1])) {
+                   for(Construct c : constructList)
+                       if(c.getName().equals(lineReader[1]))
+                           c.addToList(lineReader);
+                } else {
+                    constructStringList.add(lineReader[1]);
+                    constructList.add(new Construct(lineReader[1]));
+                    for(Construct c : constructList)
+                        if(c.getName().equals(lineReader[1]))
+                            c.addToList(lineReader);
+                }
+            }
+            for(Construct c : constructList)
+                c.printScoreList();
+
+        } finally {
+
+        }
     }
+
 }
+
