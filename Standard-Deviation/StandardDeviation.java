e9b2beb8122ab29b3321858438893a62c07569b7
Dustin Walker <duwalker88@gmail.com>
2015-07-25 22:44:58 -0400

    Cleaned up code on the standard deviation file.

--- Standard-Deviation/StandardDeviation.java
+++ Standard-Deviation/StandardDeviation.java
@@ -1,26 +1,21 @@
 import java.util.ArrayList;
 
 public class StandardDeviation {
-
-    public static void main(String[] args) {
-        ArrayList<Integer> dataSet = new ArrayList<Integer>();
-        for(int i = 1; i <= 5; i++) dataSet.add(i);
-          System.out.println(dataSet.toString());
-        StandardDeviation stddv = new StandardDeviation();
-        System.out.println(stddv.standardDeviationUsingVariance(dataSet));
-        /*These two lines print out:
-            [1, 2, 3, 4, 5]
-            1.4142135623730951
-        */
+     /**
+     * Standard deviation can be calculated as the square root of the variance as demonstrated here
+     * @param inputList List of integers to calculate standard deviation
+     * @return Standard deviation of list
+     */
+    public double standardDeviation(ArrayList<Integer> inputList){
+        return Math.sqrt(variance(inputList));
     }
 
-    public double standardDeviationUsingVariance(ArrayList<Integer> inputList){
-        return Math.pow(twoPassVariance(inputList), 0.5);
-    }
-
-    //Algorithm obtained from this page
-    //http://en.wikipedia.org/wiki/Algorithms_for_calculating_variance
-    public double twoPassVariance(ArrayList<Integer> inputList){
+    /**
+     * Method returns the variance when given an arrayList of integers.
+     * @param inputList List of integers to calculate variance
+     * @return Variance of list
+     */
+    public double variance(ArrayList<Integer> inputList){
         int n=0, sum1=0, sum2=0;
         for(int i: inputList){
             n+=1;
